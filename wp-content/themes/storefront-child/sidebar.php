7aef2df73ea09469bcb227517b6d6be0d624cd17
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-03-19 16:56:56 +0300

    вывод книг в сборнике рассказов

--- wp-content/themes/storefront-child/sidebar.php
+++ wp-content/themes/storefront-child/sidebar.php
@@ -34,9 +34,16 @@ if (!is_active_sidebar('sidebar-1')) {
 
     <div class="sidebar-cycles">
         <p class="sidebar-cycles__header">Сборники рассказов</p>
-        <?php foreach ($product_categories as $cat):?>
-            <?php if ($cat->slug === 'storybooks'): ?>
-                <p class="sidebar-cycles__item"><a class="link" href="#<?= $cat->slug ?>"><?= $cat->name ?></a</p>
+        <?php foreach ($product_categories as $cat): ?>
+            <?php if ($cat->slug === 'storybooks'):
+                $productsCat = wc_get_products(array(
+                    'category' => array($cat->slug),
+                ));
+                foreach ($productsCat as $productCat):
+                    ?>
+                    <p class="sidebar-cycles__item"><a class="link"
+                                                       href="#<?= $cat->slug ?>"><?= $productCat->name ?></a</p>
+                <?php endforeach; ?>
             <?php endif; ?>
         <?php endforeach; ?>
     </div>
@@ -46,7 +53,8 @@ if (!is_active_sidebar('sidebar-1')) {
             <p class="sidebar-attention__header">Внимание</p>
             <img src="/wp-content/themes/storefront-child/svg/svg-attention.svg" alt="svg-attention">
             <p>Здесь размещены только полные тексты моих книг.</p>
-            <p>Для скачивания нажмите на книгу и выберите формат. Если что-то пошло не так, пишите на <a href="mailto:maria@sakrytina.ru">maria@sakrytina.ru</a></p>
+            <p>Для скачивания нажмите на книгу и выберите формат. Если что-то пошло не так, пишите на <a
+                        href="mailto:maria@sakrytina.ru">maria@sakrytina.ru</a></p>
             <p>Скачивание моих книг на пиратских сайтах вредит вашей карме и компьютеру. Остерегайтесь мошенников!</p>
         </div>
     </div>
