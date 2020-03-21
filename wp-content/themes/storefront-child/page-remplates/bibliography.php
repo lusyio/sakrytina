ee488a0b292d2d55231261970d79a1589e5157c3
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-21 19:04:15 +0300

    укороченный первый border для книги в библиографии

--- wp-content/themes/storefront-child/page-remplates/bibliography.php
+++ wp-content/themes/storefront-child/page-remplates/bibliography.php
@@ -36,11 +36,12 @@ Template Post Type: post, page, product
 
                 $book_year = (get_post_meta($idBook, 'book_year', true));
 
-                $yearTerms[] = (int) $book_year;
+                $yearTerms[] = (int)$book_year;
 
             }
             $yearTerms = array_unique($yearTerms);
             sort($yearTerms);
+            $countBLock = 1;
 
             foreach (array_reverse($yearTerms) as $yearNumber):
                 $args = array(
@@ -58,7 +59,7 @@ Template Post Type: post, page, product
                 <div class="row">
                     <div class="col-1 mb-auto" style="margin-top: 6.5%;"><p
                                 class="bibliography__year"><?= $yearNumber ?></p></div>
-                    <div class="col bibliography__border">
+                    <div class="col bibliography__border <?php if($countBLock == 1) : echo 'bibliography__border1'; endif;?>">
                         <?php
                         $i = 0;
                         foreach ($products as $sortedProd):
@@ -70,77 +71,86 @@ Template Post Type: post, page, product
                             $book_year = (get_post_meta($idBook, 'book_year', true));
 
                             // сравниванием года
-                        
-                            if($yearNumber==$book_year) :
-                            // получаем картинку
-                            $imgsrc = wp_get_attachment_url($sortedProd->get_image_id());
-                            if (empty($imgsrc)) :
-                            $imgsrc = '/wp-content/uploads/woocommerce-placeholder.png';
-                            endif;
 
-                            // жанры
-                            $tags = get_the_terms($idBook, 'product_tag');
-                            foreach ($tags as $tag) {
-                                $tagNameList[] = $tag->name;
-                            }
-
-                            // цикл
-                            $catTerms = get_the_terms($idBook, 'product_cat');
-                            foreach ($catTerms as $key => $term):
-                                $catName = $term->name;
-                                $linkCat = '/shop/#' . $term->slug;
-                            endforeach;
-
-                            // награда
-                            $award = (get_post_meta($idBook, 'award', true));
+                            if ($yearNumber == $book_year) :
 
 
-                            ?>
-                            <div class="row bibliography__row">
-                                <div class="col-1 m-auto">
-                                    <?php if ($yearNumber && $i === 0): ?>
-                                        <img class="bibliography__img" src="/wp-content/themes/storefront-child/svg/svg-point.svg" alt="point">
-                                    <?php else: ?>
-                                        <img class="bibliography__img" src="/wp-content/themes/storefront-child/svg/svg-point-outline.svg"
-                                             alt="point">
-                                    <?php endif; ?>
-                                </div>
-                                <div class="col-2">
-                                    <a href="<?= $sortedProd->get_permalink() ?>">
-                                        <div class="bibliography__thumbnail">
-                                            <img alt="<?= $sortedProd->name ?>"
-                                                 src="<?=$imgsrc;?>"/>
+                                // получаем картинку
+                                $imgsrc = wp_get_attachment_url($sortedProd->get_image_id());
+                                if (empty($imgsrc)) :
+                                    $imgsrc = '/wp-content/uploads/woocommerce-placeholder.png';
+                                endif;
+
+                                // жанры
+                                $tags = get_the_terms($idBook, 'product_tag');
+                                foreach ($tags as $tag) {
+                                    $tagNameList[] = $tag->name;
+                                }
+
+                                // цикл
+                                $catTerms = get_the_terms($idBook, 'product_cat');
+                                foreach ($catTerms as $key => $term):
+                                    $catName = $term->name;
+                                    $linkCat = '/shop/#' . $term->slug;
+                                endforeach;
+
+                                // награда
+                                $award = (get_post_meta($idBook, 'award', true));
+
+
+                                ?>
+                                <div class="row bibliography__row">
+                                    <div class="col-1 m-auto">
+                                        <?php if ($yearNumber && $i === 0): ?>
+                                            <img class="bibliography__img"
+                                                 src="/wp-content/themes/storefront-child/svg/svg-point.svg"
+                                                 alt="point">
+                                        <?php else: ?>
+                                            <img class="bibliography__img"
+                                                 src="/wp-content/themes/storefront-child/svg/svg-point-outline.svg"
+                                                 alt="point">
+                                        <?php endif; ?>
+                                    </div>
+                                    <div class="col-2">
+                                        <a href="<?= $sortedProd->get_permalink() ?>">
+                                            <div class="bibliography__thumbnail">
+                                                <img alt="<?= $sortedProd->name ?>"
+                                                     src="<?= $imgsrc; ?>"/>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col-6 bibliography__card">
+                                        <div>
+                                            <p class="bibliography__title"><a
+                                                        href="<?= $sortedProd->get_permalink() ?>"><?= $sortedProd->name ?> <?=$countBLock;?></a>
+                                            </p>
+                                            <hr>
+                                            <p class="bibliography__genre"><?php foreach ($tagNameList as $key => $genre) {
+                                                    echo $genre;
+                                                    echo (count($tagNameList) - 1 !== $key) ? ', ' : '';
+                                                } ?></p>
+                                            <p class="bibliography__cycle"><a
+                                                        href="<?= $linkCat ?>"><?= $catName; ?></a></p>
                                         </div>
-                                    </a>
-                                </div>
-                                <div class="col-6 bibliography__card">
-                                    <div>
-                                        <p class="bibliography__title"><a href="<?= $sortedProd->get_permalink() ?>"><?= $sortedProd->name ?></a></p>
-                                        <hr>
-                                        <p class="bibliography__genre"><?php foreach ($tagNameList as $key => $genre) {
-                                                echo $genre;
-                                                echo (count($tagNameList) - 1 !== $key) ? ', ' : '';
-                                            } ?></p>
-                                        <p class="bibliography__cycle"><a href="<?= $linkCat ?>"><?=$catName;?></a></p>
+                                    </div>
+                                    <div class="col-3 mt-auto mb-auto">
+                                        <?php if (!empty($award)) :
+                                            ?>
+                                            <div class="bibliography__award">
+                                                <img src="/wp-content/themes/storefront-child/images/img-award-bibl.png"
+                                                     alt="award">
+                                                <p><?= $award; ?></p>
+                                            </div>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
-                                <div class="col-3 mt-auto mb-auto">
-                                    <?php if(!empty($award)) :
-                                        ?>
-                                        <div class="bibliography__award">
-                                            <img src="/wp-content/themes/storefront-child/images/img-award-bibl.png"
-                                                 alt="award">
-                                            <p><?=$award;?></p>
-                                        </div>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
 
-                            <?php
-                            $tagNameList = array();
-                            $i++;
-endif;
-                            endforeach; ?>
+                                <?php
+                                $tagNameList = array();
+                                $i++;
+                                $countBLock++;
+                            endif;
+                        endforeach; ?>
                     </div>
                 </div>
             <?php endforeach; ?>
