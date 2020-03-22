6c269740afbb34d26e739b88eb162460d18cfb97
Igor Garifyanov <garifianov.i@gmail.com>
2020-03-22 21:49:09 +0300

    Добавил вывод рассказов для книг с категорией sbornik-rasskazov

--- wp-content/themes/storefront-child/woocommerce.php
+++ wp-content/themes/storefront-child/woocommerce.php
@@ -24,44 +24,54 @@ get_header(); ?>
                 comments_template();
             } ?>
         </main><!-- #main -->
-        <div class="product-card d-none">
+            <?php if (is_product()): ?>
+                <?php
+                    global $post;
+                    $showList = false;
+                    $terms = get_the_terms( $post->ID, 'product_cat' );
+                    foreach ($terms as $term) {
+                        if ($term->slug == 'sbornik-rasskazov') {
+                            $showList = true;
+                            break;
+                        }
+                    }
+                ?>
+                <?php
+                    if (isset($showList) && $showList):
+                        $articles = [];
+                        $afterMore = preg_split('~<!--more-->~', $post->post_content, 2);
+                        if (count($afterMore) > 1) {
+                            preg_match_all('~<li>(.+)</li>~mU', $afterMore[1], $articles);
+                        }
+                        $firstColumnCount = 0;
+                        if (count($articles) > 1):
+                            $articlesNumber = count($articles[1]);
+                            $firstColumnCount = ceil($articlesNumber / 2);
+                        ?>
+        <div class="product-card">
             <div class="product-card__main-body">
                 <h4 class="text-center">Рассказы в сборнике</h4>
                 <div class="blockStory">
                     <div class="row">
+                            <?php foreach ($articles[1] as $key => $articleName):?>
+                                <?php if ($key == 0 || $key == $firstColumnCount): ?>
                         <div class="col-sm-6">
                             <ul>
-                                <li>«Вечная молодость»</li>
-                                <li>«Вьюга»</li>
-                                <li>«Мальчик по вызову»</li>
-                                <li>«Я буду любить тебя вечно»</li>
-                                <li>«Не к той блондинке сунулся!»</li>
-                                <li>«Всё будет хорошо, детка!»</li>
-                                <li>«А вы верите в сказки?»</li>
-                                <li>«Легенда»</li>
-                                <li>«Взлетай»</li>
-                                <li>«Моя Снежинка»</li>
+                                <?php endif; ?>
+                                <li><?php echo $articleName ?></li>
+                                <?php if ($key == $firstColumnCount - 1 || $key == $articlesNumber): ?>
                             </ul>
                         </div>
-                        <div class="col-sm-6">
-                            <ul>
-                                <li>«Лунный идальго»</li>
-                                <li>«Он и Она»</li>
-                                <li>«Скрипка»</li>
-                                <li>«Тающий лёд»</li>
-                                <li>«Любовь зла»</li>
-                                <li>«Я всё ещё здесь»</li>
-                                <li>«Я закрываю глаза»</li>
-                                <li>«Скажи да мне или нет»</li>
-                                <li>«Приворот»</li>
-                            </ul>
-                        </div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
                     </div>
-
                 </div>
             </div>
         </div>
-    </section><!-- #primary -->
+                        <?php endif; ?>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </section><!-- #primary -->
 <?php else: ?>
     <section id="primary" class="content-area col-sm-12 col-lg-8 archive-product-page">
         <main id="main" class="site-main" role="main">
