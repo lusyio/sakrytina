a73188eb97d7fc31666ec2588d061fc1d2b63f69
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-03-23 18:12:30 +0300

    Update variation-add-to-cart-button.php

--- wp-content/themes/storefront-child/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wp-content/themes/storefront-child/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -10,20 +10,31 @@
 defined('ABSPATH') || exit;
 
 global $product;
+$is_in_cart = false;
+
+foreach (WC()->cart->get_cart() as $cart_item)
+    if ($cart_item['product_id'] == $product->get_id()) {
+        $is_in_cart = true;
+        break;
+    }
+
+if ($is_in_cart)
+    $button_text = __('Уже в корзине', 'woocommerce');
+
 ?>
-<?php //if (products_in_cart()): ?>
-<!--    <a href="--><?php //echo get_permalink(wc_get_page_id('cart')); ?><!--"-->
-<!--       class="btn btn-primary">Товар в корзине</a>-->
-<?php //else: ?>
+<?php if ($is_in_cart): ?>
+    <a class="btn btn-primary" href="/cart/">Уже в корзине</a>
+<?php else: ?>
     <div class="woocommerce-variation-add-to-cart variations_button">
         <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
         <button type="submit"
-                class="btn btn-primary "><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+                class="btn btn-primary single_add_to_cart_button"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
 
         <?php do_action('woocommerce_after_add_to_cart_button'); ?>
 
         <input type="hidden" name="add-to-cart" value="<?php echo absint($product->get_id()); ?>"/>
         <input type="hidden" name="product_id" value="<?php echo absint($product->get_id()); ?>"/>
     </div>
-<?php //endif; ?>
+<?php endif; ?>
+
