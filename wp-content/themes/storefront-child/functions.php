f99f7a64394cf670a38896d670c3f93bea1787b2
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-03-23 17:33:50 +0300

    добавление в корзину ajax single product

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -884,3 +884,58 @@ function mytheme_customize_register($wp_customize)
         )
     );
 }
+
+function ql_woocommerce_ajax_add_to_cart_js()
+{
+
+    if (function_exists('is_product') && is_product()) {
+        wp_enqueue_script('add-to-cart-single_ajax', get_stylesheet_directory_uri() . '/inc/assets/js//add-to-cart-single.js', array('jquery'), '', true);
+    }
+}
+
+add_action('wp_enqueue_scripts', 'ql_woocommerce_ajax_add_to_cart_js');
+
+add_action('wp_ajax_ql_woocommerce_ajax_add_to_cart', 'ql_woocommerce_ajax_add_to_cart');
+
+add_action('wp_ajax_nopriv_ql_woocommerce_ajax_add_to_cart', 'ql_woocommerce_ajax_add_to_cart');
+
+function ql_woocommerce_ajax_add_to_cart()
+{
+
+    $product_id = apply_filters('ql_woocommerce_add_to_cart_product_id', absint($_POST['product_id']));
+
+    $quantity = empty($_POST['quantity']) ? 1 : wc_stock_amount($_POST['quantity']);
+
+    $variation_id = absint($_POST['variation_id']);
+
+    $passed_validation = apply_filters('ql_woocommerce_add_to_cart_validation', true, $product_id, $quantity);
+
+    $product_status = get_post_status($product_id);
+
+    if ($passed_validation && WC()->cart->add_to_cart($product_id, $quantity, $variation_id) && 'publish' === $product_status) {
+
+        do_action('ql_woocommerce_ajax_added_to_cart', $product_id);
+
+        if ('yes' === get_option('ql_woocommerce_cart_redirect_after_add')) {
+
+            wc_add_to_cart_message(array($product_id => $quantity), true);
+
+        }
+
+        WC_AJAX:: get_refreshed_fragments();
+
+    } else {
+
+        $data = array(
+
+            'error' => true,
+
+            'product_url' => apply_filters('ql_woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id));
+
+        echo wp_send_json($data);
+
+    }
+
+    wp_die();
+
+}
