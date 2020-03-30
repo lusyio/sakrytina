1fa140e7601135b78ff4ab9256b49fd40fb3821d
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2020-03-30 17:49:28 +0300

    yandex webmaster

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -13,6 +13,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="yandex-verification" content="34c631cccf638b5a" />
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php wp_head(); ?>
